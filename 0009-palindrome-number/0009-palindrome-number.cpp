d4d51bf2292a49366d657b7d58f33bca61b075a2
Deepak Pandey <pandeydeepak312005@gmail.com>
2026-06-27 00:30:32 +0530

    Time: 2 ms (51.04%), Space: 8.5 MB (65.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long rev=0,y=(long)x;
+        if(x<0)
+        return false;
+        else
+        {
+            while(y!=0)
+            {
+                rev=(rev*10)+(y%10);
+                y/=10;
+            }
+            if(rev==x)
+            return true;
+            else
+            return false;
+        }
+    }
+};
