bcb200bc2a0313226ad4de6876317b0b447f5be8
Deepak Pandey <pandeydeepak312005@gmail.com>
2026-06-30 23:59:27 +0530

    Time: 45 ms (24.83%), Space: 47.1 MB (39.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,7 +5,7 @@ class Solution {
         {
             for(j=i+1; j<nums.length; j++)
             {
-                if((nums[i]-target)+nums[j]==0)//comparing
+                if((nums[i]-target)+nums[j]==0)//comparin
                 {
                     i+=nums.length;
                     break;
