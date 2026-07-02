efff982118a6279d5adb4f57be4c9fb29b586174
Deepak Pandey <pandeydeepak312005@gmail.com>
2026-07-02 23:38:40 +0530

    Time: 0 ms (100%), Space: 43.1 MB (34.09%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();//removing spaces
+        s=s.trim();
         int c=0;
        for (int i=s.length()-1; i>=0; i--)
        {
